372d1f6416cf5514a8947cb93a582862bceb29c8
zhouchaoyu1 <zcy160521@163.com>
2023-06-19 15:10:51 +0800

    延时队列demo

---
+++ src/main/java/com/cz/delayqueue/consumer/DeadLetterQueueConsumer.java
@@ -0,0 +1,24 @@
+package com.cz.delayqueue.consumer;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * @author zhouchaoyu
+ * @time 2023-06-15-14:52
+ */
+@Slf4j
+@Component
+public class DeadLetterQueueConsumer {
+    @RabbitListener(queues = "QD")
+    public void receiveD(Message message, Channel channel) throws IOException {
+        String msg = new String(message.getBody());
+        log.info("当前时间：{},收到死信队列信息{}", new Date().toString(), msg);
+    }
+}
---
+++ src/main/java/com/cz/delayqueue/controller/SendMsgController.java
@@ -0,0 +1,32 @@
+package com.cz.delayqueue.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+/**
+ * 消息生产者代码
+ * @author zhouchaoyu
+ * @time 2023-06-15-14:45
+ */
+@Slf4j
+@RequestMapping("ttl")
+@RestController
+public class SendMsgController {
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("sendMsg/{message}")
+    public void sendMsg(@PathVariable String message){
+        log.info("当前时间：{},发送一条信息给两个 TTL 队列:{}", new Date(), message);
+        rabbitTemplate.convertAndSend("X", "XA", "消息来自 ttl 为 10S 的队列: "+message);
+        rabbitTemplate.convertAndSend("X", "XB", "消息来自 ttl 为 40S 的队列: "+message);
+    }
+}
