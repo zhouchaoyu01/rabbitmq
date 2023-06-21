b41985166c65008b557ca04a4bddd740cf5cfe97
zhouchaoyu1 <zcy160521@163.com>
2023-06-21 15:46:45 +0800

    延时队列优化

--- src/main/java/com/cz/delayqueue/controller/SendMsgController.java
+++ src/main/java/com/cz/delayqueue/controller/SendMsgController.java
@@ -13,6 +13,12 @@ import java.util.Date;
 
 /**
  * 消息生产者代码
+ *
+ *
+ * http://localhost:8080/ttl/sendExpirationMsg/你好 1/20000
+ * http://localhost:8080/ttl/sendExpirationMsg/你好 2/2000
+ *
+ *
  * @author zhouchaoyu
  * @time 2023-06-15-14:45
  */
@@ -24,9 +30,18 @@ public class SendMsgController {
     private RabbitTemplate rabbitTemplate;
 
     @GetMapping("sendMsg/{message}")
-    public void sendMsg(@PathVariable String message){
+    public void sendMsg(@PathVariable String message) {
         log.info("当前时间：{},发送一条信息给两个 TTL 队列:{}", new Date(), message);
-        rabbitTemplate.convertAndSend("X", "XA", "消息来自 ttl 为 10S 的队列: "+message);
-        rabbitTemplate.convertAndSend("X", "XB", "消息来自 ttl 为 40S 的队列: "+message);
+        rabbitTemplate.convertAndSend("X", "XA", "消息来自 ttl 为 10S 的队列: " + message);
+        rabbitTemplate.convertAndSend("X", "XB", "消息来自 ttl 为 40S 的队列: " + message);
+    }
+
+    @GetMapping("sendExpirationMsg/{message}/{ttlTime}")
+    public void sendMsg(@PathVariable String message, @PathVariable String ttlTime) {
+        rabbitTemplate.convertAndSend("X", "XC", message, correlationData -> {
+            correlationData.getMessageProperties().setExpiration(ttlTime);
+            return correlationData;
+        });
+        log.info("当前时间：{},发送一条时长{}毫秒 TTL 信息给队列 C:{}", new Date(), ttlTime, message);
     }
 }
